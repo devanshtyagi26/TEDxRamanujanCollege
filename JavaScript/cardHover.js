b80d240e4b2167d15f9e9ceeeb95a2ba7feeb3b7
devanshtyagi26 <tyagidevansh3@gmail.com>
2024-07-18 21:29:41 +0530

    effect added to speaker block

---
+++ JavaScript/cardHover.js
@@ -0,0 +1,13 @@
+const handleOnMouseMove = e => {
+  const { currentTarget: target } = e;
+
+  const rect = target.getBoundingClientRect(),
+    x = (e.clientX - rect.left),
+    y = (e.clientY - rect.top);
+  target.style.setProperty("--mouse-x", `${x}px`);
+  target.style.setProperty("--mouse-y", `${y}px`);
+};
+
+for (card of document.querySelectorAll(".block")) {
+  card.onmousemove = (e) => handleOnMouseMove(e);
+}
